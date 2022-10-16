f6424c75e286216dd51859fd6a9c0897dfd12ea2
jun-hf <fong.junh@gmail.com>
2022-10-16 22:13:59 +0100

    Code [Stockdemo class]

---
+++ StockDemo.java
@@ -0,0 +1,21 @@
+public class StockDemo {
+    private StockManager manager;
+
+    public StockDemo() {
+        manager = new StockManager();
+        manager.addProduct(new Product(132, "Clock Radio"));
+        manager.addProduct(new Product(37,  "Mobile Phone"));
+        manager.addProduct(new Product(23,  "Microwave Oven"));
+    }
+
+    public void demo() {
+        manager.printProductDetails();;
+        manager.delivery(132, 5);;
+        manager.printProductDetails();
+    }
+
+    public static void main(String[] args) {
+        StockDemo stockDemo1 = new StockDemo();
+        stockDemo1.demo();
+    }
+}
